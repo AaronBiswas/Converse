8f5e1641f2653f1ce6856ce364bd840d27134635
Aaron Biswas <savybiswas15@gmail.com>
2025-04-01 17:21:38 +0530

    Added login function

--- backend/Controllers/Auth.Controller.js
+++ backend/Controllers/Auth.Controller.js
@@ -70,6 +70,12 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {
-  res.send("Logout route");
+export const logout = async(req, res) => {
+  try {
+    res.cookie("jwt","",{maxAge:0})
+    return res.status(200).json({message:"Logged out successfully"});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
